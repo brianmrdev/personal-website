f0a0701da11c912a056340430cd6728b63f3bf48
Brian Martínez Rodríguez <brianmr1128@gmail.com>
2022-10-29 18:01:10 -0400

    change meta tags

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Brian Martínez Rodríguez | Personal Website';
-export const SITE_DESCRIPTION = 'Personal website of Brian Martínez Rodríguez';
+export const SITE_TITLE = 'Brian Martínez Rodríguez, Backend Web Developer';
+export const SITE_DESCRIPTION = 'Personal website of Brian Martínez Rodríguez, Backend Web Developer';
 export const URL_BASE = 'https://webdev.cu/';
