35de8f5b1314fe8af0074712678afda84c433070
Brian <brianmr1128@gmail.com>
2024-11-04 23:46:36 -0500

    feature: agregar configuración para generar sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 
-//import cloudflare from '@astrojs/cloudflare';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  //output: 'static',
-  //adapter: cloudflare(),
+  site: 'https://brianmartinez.dev',
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ],
 });
